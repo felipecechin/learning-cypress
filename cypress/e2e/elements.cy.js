4b7aad2b13afdaf381d6d2fad6abf1adab6dd449
Felipe Cechin Mello <ficechin@hotmail.com>
2023-01-19 00:00:03 -0300

    added elements test

---
+++ cypress/e2e/elements.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('Work with basic elements', () => {
+    beforeEach(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    it('Text', () => {
+        cy.get('body').should('contain', 'Cuidado')
+        cy.get('span').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
+    })
+
+    it('Links', () => {
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+
+        cy.reload()
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+    })
+})
